9c5c39474f931b66ecdb0373436dd7bd08909e45
Asheesh01 <ka301423@gmail.com>
2026-04-02 22:40:44 +0530

    Add changes

---
+++ dist/index.js
@@ -0,0 +1,103 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import { connectDB, ContentModel, UserModel } from './db.js';
+const app = express();
+app.use(express.json());
+dotenv.config();
+function authMiddleware(req, res, next) {
+    try {
+        const token = req.headers.authorization;
+        if (!token) {
+            return res.status(401).json({ message: "Token missing" });
+        }
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.userId = decoded.id;
+        next();
+    }
+    catch (err) {
+        return res.status(403).json({ message: "Invalid token" });
+    }
+}
+app.post("/signup", async (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        const HashPasword = await bcrypt.hash(password, 10);
+        await UserModel.create({
+            username: username,
+            password: HashPasword
+        });
+        res.json({
+            message: "User sign up"
+        });
+    }
+    catch (error) {
+        res.status(411).json({
+            message: "User already exists"
+        });
+    }
+});
+app.post("/login", async (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        const user = await UserModel.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const newPass = await bcrypt.compare(password, user.password);
+        if (!newPass) {
+            return res.status(400).json({ message: "Incorrect password" });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+        res.json({
+            message: "Login Successfully", token
+        });
+    }
+    catch (error) {
+        res.status(411).json({
+            message: "Login Error"
+        });
+    }
+});
+app.post("/content", authMiddleware, async (req, res) => {
+    try {
+        const { title, link, tags } = req.body;
+        const userId = req.userId;
+        await ContentModel.create({
+            title,
+            link,
+            tags,
+            userId
+        });
+        res.json({ message: "Content created successfully" });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ mesage: "Content not created" });
+    }
+});
+app.get("/content", authMiddleware, async (req, res) => {
+    try {
+        const userID = req.userId;
+        const content = await ContentModel.find({
+            userId: userID
+        }).populate("userId", "username");
+        res.json({ content });
+    }
+    catch (error) {
+        console.log(error);
+        res.json({ message: "Not fetch Content" });
+    }
+});
+async function start() {
+    await connectDB();
+    app.listen(3000, () => {
+        console.log("🚀 Server running on port 3000");
+    });
+}
+start();
+//# sourceMappingURL=index.js.map
---
+++ src/db.ts
@@ -0,0 +1,28 @@
+import mongoose, { model, Model ,Schema } from "mongoose";
+ export async function connectDB() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/brainly");
+    console.log("✅ DB connected");
+  } catch (err) {
+    console.log("❌ DB error:", err);
+  }
+}
+const USerSchema= new Schema({
+    username:{
+        type:String,
+        unique:true
+    },
+    password:String
+})
+
+const ContentSchema=new Schema({
+  title:String,
+  link:String,
+  tags:[{type:mongoose.Types.ObjectId,ref:'Tag'}],
+  userId:{type:mongoose.Types.ObjectId,ref:'User',required:true}
+})
+
+export const UserModel=model("User",USerSchema);
+
+export const ContentModel=model("Content",ContentSchema)
+
